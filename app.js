dd22f0fd34434fc4c4ed00bdefb2ccf07e6da63e
Aditya Sharma <adityas129@gmail.com>
2019-09-14 12:22:17 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/database";
+
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    // ...
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
