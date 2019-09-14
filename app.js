9468df7ef481214996ed44b46804bd2f654029a4
Aditya Sharma <adityas129@gmail.com>
2019-09-14 13:43:42 -0400

    firebase setup

--- app.js
+++ app.js
@@ -9,8 +9,27 @@ import "firebase/database";
 
 // TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-    // ...
-  };
+  apiKey: "AIzaSyCvIgWTTi16ESUYEhbQlRda9ItjXgvRgyU",
+  authDomain: "minerva-7ae74.firebaseapp.com",
+  databaseURL: "https://minerva-7ae74.firebaseio.com",
+  projectId: "minerva-7ae74",
+  storageBucket: "",
+  messagingSenderId: "1018992044633",
+  appId: "1:1018992044633:web:dcdfdb4754bde54e095f48"
+}
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+
+  function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+
+  writeUserData(123, 'adi', 'test', 'testurl')
