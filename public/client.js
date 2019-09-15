cca9937b015d281ac2d4a88a8a9c99398617fbc3
sanzeed <sanzeed@mit.edu>
2019-09-15 10:37:52 -0400

    Last moment push

---
+++ public/client.js
@@ -0,0 +1,23 @@
+console.log('Client-side code running');
+
+const button = document.getElementById('submit_btn');
+console.log('Casdlfkj');
+
+button.addEventListener('click', function(e) {
+  
+  console.log('button was clickedsanzeed' );
+
+});
+
+
+
+
+
+// button.addEventListener('')
+// a = function(e) {
+  
+//   console.log('button was clicked');
+// }
+// button.addEventListener('click', a);
+
+
--- server.js
+++ server.js
@@ -3,9 +3,8 @@ var ip_address = '18.20.216.61';
 var http = require("http");
 var fs = require('fs');
 
+
 var firebase = require('firebase');
-var admin = require('firebase-admin');
-var funcs = require('firebase-functions');
 
 var firebaseConfig = {
   apiKey: "AIzaSyCvIgWTTi16ESUYEhbQlRda9ItjXgvRgyU",
@@ -20,50 +19,167 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 let db = firebase.firestore();
-db.collection('cats').get()
-  .then((snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
-  })
-  .catch((err) => {
-    console.log('Error getting documents', err);
-  });
+
+
+// db.collection('cats').get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, '=>', doc.data());
+//       console.log("ldsakfjjasdfjlfsd")
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
 
 
 
-  //this is how you write to db!!!
-  let docRef = db.collection('cats').doc('writing_first_time');
+//   //this is how you write to db!!!
+//   let docRef = db.collection('cats').doc('writing_first_time');
+//   let writing_first_time = docRef.set({
+//     'Adi': 'bad',
+//     'bad': 15,
+//     'good': 10, 
+//     predicted_label: 'bad'
+//   });  npm install express --save
+
+
+
+
+
+// http.createServer(function (request, response) {
+//    // Send the HTTP header 
+//    // HTTP Status: 200 : OK
+//    // Content Type: text/plain
+//    response.writeHead(200, {'Content-Type': 'text/html'});
+//    // Send the response body as "Hello World"
+//    // response.end('Hello World\n');
+
+//    fs.readFile('./upload.html', null, function (error, data) {
+//         if (error) {
+//             response.writeHead(404);
+//             respone.write('Whoops! File not found!');
+//         } else {
+//             response.write(data);
+
+//             // response.write(data);
+//         }
+//         response.end();
+//     });
+
+// }).listen(3000, ip_address);
+
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
+
+
+
+console.log('Server-side code running');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+// start the express web server listening on 8080
+app.listen(8080, () => {
+  console.log('listening on 8080');
+});
+
+// serve the homepage
+app.get('/', (req, res) => {
+
+  res.sendFile(__dirname + '/welcome.html');
+});
+
+
+app.get('/clicked', (req, res) => {
+  const click = {clickTime: new Date()};
+  console.log(click);
+  console.log("kkmkmmkm");
+
+  // db.collection('data1').doc('express-test');
+  let docRef = db.collection('test').doc()
+
   let writing_first_time = docRef.set({
     'Adi': 'bad',
     'bad': 15,
     'good': 10, 
     predicted_label: 'bad'
-  });  
+  });
+  res.sendFile(__dirname + '/labelling_session.html');
+
+});
+
+app.post('/submit', (req, res) => {
+  console.log("kkmkmmkm");
+  res.sendFile(__dirname + '/labelling_session.html');
+
+});
+var path = process.cwd();
+
+app.route('/uploading')
+ 	.get(function (req, res) {
+      
+    res.sendFile(__dirname + '/upload.html');
+	});
+
+  app.route('/login')
+ 	.get(function (req, res) {
+      
+    res.sendFile(__dirname + '/login.html');
+	});
 
 
+app.route('/goData')
+ 	.get(function (req, res) {
 
-
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/html'});
-   
-   // Send the response body as "Hello World"
-   // response.end('Hello World\n');
-
-   fs.readFile('./sample_html.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
+var sentiment = {negative_c: 0, positive_c:0, neutral_c:0}
+var ejs_i = 0;
+var data_json = [{},sentiment, ejs_i];
+var index = 0;
+      
+db.collection('data1').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      sentiment.negative_c = sentiment.negative_c + parseInt(JSON.stringify(doc.data().label_distribution.Negative));
+      sentiment.neutral_c = sentiment.neutral_c + parseInt(JSON.stringify(doc.data().label_distribution.Neutral));
+      sentiment.positive_c = sentiment.positive_c + parseInt(JSON.stringify(doc.data().label_distribution.Positive));
     });
+})
+  
 
-}).listen(3000, ip_address);
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+      
+  console.log("outside fo the db read");
+  
+  db.collection('data1').get()
+    .then((snapshot) => {
+      console.log("in the snapshot");
+      
+      snapshot.forEach((doc) => {
+        data_json[0][index] = doc.data();
+        index++;
+        // console.log('bruh', data_json.data)
+        // options = data_json.label_distribution
+        // const html_gen = window.document.createElement('<option value="label_option_3">${options}</option>')
+        // console.log(html_gen)
+      });
+      
+      app.set('view engine', 'ejs');
+      console.log(Object.keys(data_json[0]).length);
+      res.render(path + '/labelling_session.ejs', {dynamic : data_json});
+      })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+});
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+
+
+  
